9eb7b3838f4e040f04b89e1d7bf63d115c9c42e8
oppalaa <oppala7@gmail.com>
2023-07-22 01:12:57 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 def registration(request):
     d={'usfo':UserForm(),'pfo':ProfileForm()}
@@ -13,7 +16,6 @@ def registration(request):
             submittedPassword=usfd.cleaned_data['password']
             NSUFO.set_password(submittedPassword)
             NSUFO.save()
-            
             NSPO=pfd.save(commit=False)
             NSPO.username=NSUFO
             NSPO.save()
@@ -26,3 +28,35 @@ def registration(request):
                       fail_silently=False )
             return HttpResponse('registration form is successful check in admin')        
     return render(request,'registration.html',d)
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO:
+            if AUO.is_active:
+                login(request,AUO)
+                request.session['username']=username
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('Not a Active User')
+        else:
+            return HttpResponse('Invalid Details')
+    return render(request,'user_login.html')
+
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
